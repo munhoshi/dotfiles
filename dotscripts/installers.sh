db5c021aef82a94b9a59ebc07fd4c909632dcda8
songpixi <98211231+songpixi@users.noreply.github.com>
2022-02-13 10:43:24 -0600

    Add lolcat and cowsay to packages

--- dotscripts/installers.sh
+++ dotscripts/installers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Stuff to install explicitly:
-sudo pamac install --no-confirm vim neofetch ytfzf mpv yt-dlp falkon signal-desktop telegram-desktop spotify frozen-bubble kompare kvantum imagewriter
+sudo pamac install --no-confirm vim neofetch lolcat cowsay ytfzf mpv yt-dlp falkon signal-desktop telegram-desktop spotify frozen-bubble kompare kvantum imagewriter
 
 #Stuff to install --as-deps
 sudo pamac install --no-confirm --as-deps falkon-pdfreader-git jq fzf ueberzug
