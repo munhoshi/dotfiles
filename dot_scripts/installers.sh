cb6e95336b224c0de8ec8b28062d180813220893
songpixi <98211231+songpixi@users.noreply.github.com>
2022-02-13 08:16:53 -0600

    Add scripts to sync dotfiles and install packages

---
+++ dot_scripts/installers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Stuff to install explicitly:
+sudo pamac install --no-confirm neofetch ytfzf mpv yt-dlp falkon signal-desktop telegram-desktop spotify frozen-bubble kompare kvantum imagewriter
+
+#Stuff to install --as-deps
+sudo pamac install --no-confirm --as-deps falkon-pdfreader-git jq fzf ueberzug
+
+#pip upgrade
+pip install --upgrade pip
