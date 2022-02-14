8377b1f7c2b0a1c0c0c351048881e98b50421bc0
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-02-14 00:50:26 -0600

    Add more programs, install via txt files

--- dotscripts/installers.sh
+++ dotscripts/installers.sh
@@ -4,10 +4,8 @@
 sudo echo "session		optional	pam_gnome_keyring.so auto_start" >> /etc/pam.d/sddm
 
 #Stuff to install explicitly:
-sudo pamac install --no-confirm vim neofetch lolcat cowsay ytfzf mpv yt-dlp falkon signal-desktop telegram-desktop spotify frozen-bubble kompare kvantum imagewriter
-
+sudo pamac install --no-confirm $(cat packages.txt)
 #Stuff to install --as-deps
-sudo pamac install --no-confirm --as-deps falkon-pdfreader-git jq fzf ueberzug
-
+sudo pamac install --no-confirm $(cat deps.txt)
 #pip upgrade
 pip install --upgrade pip
