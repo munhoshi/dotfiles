044179827790ad479c5ee3259e9268b6b4c3c9c4
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-02-13 20:46:26 -0600

    Add line to automatically unlock gnome-keyring

--- dotscripts/installers.sh
+++ dotscripts/installers.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Fix keyring:
+sudo echo "session		optional	pam_gnome_keyring.so auto_start" >> /etc/pam.d/sddm
+
 #Stuff to install explicitly:
 sudo pamac install --no-confirm vim neofetch lolcat cowsay ytfzf mpv yt-dlp falkon signal-desktop telegram-desktop spotify frozen-bubble kompare kvantum imagewriter
 
