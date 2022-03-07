a9768911088ebf94269baa98ca224557d715eb08
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-03-06 23:21:54 -0600

    Remove unneeded lines from sync_dotfiles

--- dotscripts/sync_dotfiles.sh
+++ dotscripts/sync_dotfiles.sh
@@ -3,21 +3,21 @@
 #Fix keyring:
 sudo echo "session		optional	pam_gnome_keyring.so auto_start" >> /etc/pam.d/sddm
 
-git clone --bare https://github.com/dotfiles/dotfiles.git $HOME/.cfg
+git clone --bare https://github.com/lumipixi/dotfiles.git $HOME/.cfg
 echo ".cfg" >> .gitignore
 
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-config checkout
+#mkdir -p .config-backup
+#config checkout
+#if [ $? = 0 ]; then
+#  echo "Checked out config.";
+#  else
+#    echo "Backing up pre-existing dot files.";
+#    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+#fi;
+config checkout -f
 config config status.showUntrackedFiles no
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
+#alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+#echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
