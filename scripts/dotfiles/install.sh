253c5bc401dab6f22b769f87ceb916f35819d3d1
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-03-20 00:54:28 -0600

    Update installation script

--- scripts/dotfiles/install.sh
+++ scripts/dotfiles/install.sh
@@ -4,7 +4,7 @@ git clone --bare https://github.com/lumipixi/dotfiles.git $HOME/.cfg
 echo ".cfg" >> .gitignore
 
 function conf {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
 conf checkout -f
