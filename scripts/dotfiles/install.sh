e4dd09d76ff45863adf1e8f54346c5d3bbec79d8
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-10-16 05:18:10 -0600

    update install script

--- scripts/dotfiles/install.sh
+++ scripts/dotfiles/install.sh
@@ -4,11 +4,7 @@ echo "Cloning dotfiles..."
 git clone --bare https://github.com/lumipixi/dotfiles.git $HOME/.cfg
 echo ".cfg" >> .gitignore
 
-function conf {
-   git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-
-conf checkout -f
-conf config status.showUntrackedFiles no
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config status.showUntrackedFiles no
 
 echo "Done! Please reload the shell"
