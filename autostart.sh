0894c11ef4e48f92d4b1fb5c5ab17a48ce2036f0
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-11-06 01:13:02 -0600

    Add AwesomeWM and GTK theme config

---
+++ autostart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function run {
+    if ! pgrep $1 ;
+    then
+        $@&
+    fi
+}
+
+nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+picom --experimental-backends -b
+nitrogen --restore
+setxkbmap -layout "us,latam" -option "grp:alt_shift_toggle"
