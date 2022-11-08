091511cb6384f4b709f36cb52f7c85d82b619b92
lumipixi <98211231+lumipixi@users.noreply.github.com>
2022-11-07 22:48:30 -0600

    Add Kitty terminal, make default in AwesomeWM

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -54,8 +54,8 @@ local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.ge
 beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
---terminal = "kitty"
+--terminal = "x-terminal-emulator"
+terminal = "kitty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
